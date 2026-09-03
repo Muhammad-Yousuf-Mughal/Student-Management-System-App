579b1969831b45d152709e8320b44f7ee746af74
Muhammad Yousuf Mughal <mughalyousuf955@gmail.com>
2026-09-04 01:14:11 +0500

    Clarify build script context in comment
    Updated the comment to specify Railway/Render context.

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
-# Render build script
+# Railway/Render build script
 set -o errexit
 
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py migrate
